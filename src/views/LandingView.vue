cba6b32432a3732cefcbd0df5de1a0e1ed734118
YangYiFan <Coolfan1024@gmail.com>
2024-06-06 21:55:06 +0800

    router的引入

---
+++ src/views/LandingView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+function handleClick() {
+    router.push('/uniboard')
+}
+</script>
+<template>
+    <h1>Landing View</h1>
+    <button @click="handleClick">to UniBoard View</button>
+</template>
---
+++ src/views/UniBoardView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router' 
+const router = useRouter()
+
+function handleClick() {
+    router.push('/')
+}
+</script>
+<template>
+    <h1>UniBoard View</h1>
+    <button @click="handleClick">to Landing View</button>
+</template>
