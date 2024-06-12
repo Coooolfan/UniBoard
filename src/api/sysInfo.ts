8421548af933200a5f1019a190edd87e63133a5c
YangYiFan <Coolfan1024@gmail.com>
2024-06-12 23:08:16 +0800

    网页标题与icon，更多的picture标签

--- src/api/sysInfo.ts
+++ src/api/sysInfo.ts
@@ -4,7 +4,21 @@ interface SystemInfo {
     name: string
     version: string
     profile: string
-    avater: string
+    avatar: string
+    slogan: string
+    banner: string
+    contacts: {
+        telegram: string
+        qq: string
+        email: string
+        github: string
+        weibo: string
+        zhihu: string
+        twitter: string
+        facebook: string
+        instagram: string
+        linkedin: string
+    }
 }
 
 interface SystemInfoItem {
@@ -13,20 +27,51 @@ interface SystemInfoItem {
     value: string
 }
 
+interface ContactItem {
+    title: string
+    content: string
+}
+
 async function getSystemInfo(): Promise<SystemInfo> {
     try {
         const response = await axiosInstance.get<Array<SystemInfoItem>>('sysInfo/')
-        console.log(response.data)
         const findValue = (name: string) => {
             const item = response.data.find((item) => item.name === name)
             return item ? item.value : ''
         }
+        const contacts: Array<ContactItem> = Object.entries(JSON.parse(findValue('contacts'))).map(
+            ([title, content]) => ({
+                title: title as string,
+                content: content as string
+            })
+        )
+        console.log('Contacts:', contacts)
+        const findContact = (name: string) => {
+            const item = contacts.find((item) => item.title === name)
+            return item ? item.content : ''
+        }
+
         const systemInfo: SystemInfo = {
             name: findValue('name'),
             version: findValue('version'),
             profile: findValue('profile'),
-            avater: findValue('avater')
+            avatar: findValue('avatar'),
+            slogan: findValue('slogan'),
+            banner: findValue('banner'),
+            contacts: {
+                telegram: findContact('telegram'),
+                qq: findContact('qq'),
+                email: findContact('email'),
+                github: findContact('github'),
+                weibo: findContact('weibo'),
+                zhihu: findContact('zhihu'),
+                twitter: findContact('twitter'),
+                facebook: findContact('facebook'),
+                instagram: findContact('instagram'),
+                linkedin: findContact('linkedin')
+            }
         }
+        console.log('System info:', systemInfo)
         return systemInfo
     } catch (error) {
         console.error(`Error occurred while fetching system info: ${error}`)
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            boxShadow: {
+                ultra: '5px 0 100px 12px' // 修改这里的值以自定义阴影
+            }
+        }
     },
-    plugins: []
+    plugins: [
+        function ({ addUtilities }) {
+            const newUtilities = {
+                '.text-shadow-s': {
+                    textShadow: '1px 1px 1px rgba(0, 0, 0, 0.5)'
+                },
+                '.text-shadow-m': {
+                    textShadow: '2px 2px 2px rgba(0, 0, 0, 0.5)'
+                },
+                '.text-shadow-l': {
+                    textShadow: '3px 3px 3px rgba(0, 0, 0, 0.5)'
+                },
+                '.text-shadow-xl': {
+                    textShadow: '4px 4px 4px rgba(0, 0, 0, 0.5)'
+                },
+                '.text-shadow-2xl': {
+                    textShadow: '5px 5px 5px rgba(0, 0, 0, 0.5)'
+                }
+            }
+            addUtilities(newUtilities)
+        }
+    ]
 }
