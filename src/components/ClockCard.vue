14177a33a69c1f4e3bbb2a593ce42507912616bb
YangYiFan <Coolfan1024@gmail.com>
2024-06-23 01:01:31 +0800

    时钟组件

---
+++ src/components/ClockCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+const time = ref(new Date())
+// 创建一个计时器，10ms更新一次time
+setInterval(() => {
+    time.value = new Date()
+}, 10)
+const time_formatted = computed(() => time.value.toLocaleTimeString())
+</script>
+<template>
+    <div
+        v-for="(item, index) in time_formatted"
+        :key="index"
+        class="min-w-16 text-center text-9xl inline-block"
+    >
+        {{ item }}
+    </div>
+</template>
--- src/views/UniBoardView.vue
+++ src/views/UniBoardView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router' 
+import { useRouter } from 'vue-router'
+import ClockCard from '@/components/ClockCard.vue'
 const router = useRouter()
 
 function handleClick() {
@@ -7,6 +8,7 @@ function handleClick() {
 }
 </script>
 <template>
+    <ClockCard />
     <h1>UniBoard View</h1>
     <button @click="handleClick">to Landing View</button>
 </template>
