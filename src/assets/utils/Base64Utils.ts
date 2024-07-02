a964ad63df28c910ec301ec41e42903fca6536ac
YangYiFan <Coolfan1024@gmail.com>
2024-07-02 22:02:45 +0800

    更好的base64转文件

--- src/assets/utils/Base64Utils.ts
+++ src/assets/utils/Base64Utils.ts
@@ -1,17 +1,26 @@
-function base64ToFile(urlData: any) {
-    // TODO 要修一下的
-    
+const MIMEMap: { [key: string]: string } = {
+    'image/vnd.microsoft.icon': 'ico',
+    'image/svg+xml': 'svg'
+}
+
+function base64ToFile(urlData: string) {
+    // TODO 缺乏测试
     let arr = urlData.split(',')
-    let mime = arr[0].match(/:(.*?);/)[1]
+    let mime = arr[0].match(/:(.*?);/)?.[1] || ''
     let bytes = atob(arr[1]) // 解码base64
     let n = bytes.length
     let ia = new Uint8Array(n)
     while (n--) {
         ia[n] = bytes.charCodeAt(n)
     }
-    let fileName = 'file_' + Date.now() + '.' + mime.split('/')[1]
-    return new File([ia], fileName, { type: mime })
+    let extension = mime.split('/')[1]
+    if (MIMEMap.hasOwnProperty(mime)) {
+        extension = MIMEMap[mime]
+    } else {
+        console.error(`Unknown MIME type: ${mime}`)
+    }
+    let fileName = 'file_' + Date.now() + '.' + extension
+    return new File([ia], fileName, { type: extension })
 }
-
 
 export { base64ToFile }
