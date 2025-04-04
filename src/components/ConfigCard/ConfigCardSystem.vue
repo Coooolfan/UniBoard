31000e15c3b6b9cf396a77b63be2aeefa83eb533
YangYiFan <coolfan1024@gmail.com>
2025-04-04 23:04:13 +0800

    refactor: 更新ConfigCardSystem组件以使用新的API结构，优化系统配置加载和密码更新逻辑，增强错误处理

--- src/components/ConfigCard/ConfigCardSystem.vue
+++ src/components/ConfigCard/ConfigCardSystem.vue
@@ -1,26 +1,47 @@
 <script lang="ts" setup>
-import { defaultSysConfig, getSysConfig, patchSysConfig, type sysConfig } from '@/api/sysConfig'
 import Button from 'primevue/button'
 import Card from 'primevue/card'
 import LabelAndInput from '@/components/LabelAndInput.vue'
 import LabelAndCheckbox from '@/components/LabelAndCheckbox.vue'
 import { onMounted, ref } from 'vue'
 import { useToast } from 'primevue/usetoast'
-import { changePassword } from '@/api/auth'
+import { api } from '@/ApiInstance'
+import type { SystemConfigDto } from '@/__generated/model/dto'
+import type { ApiErrors } from '@/__generated/ApiErrors'
+import { c } from 'node_modules/vite/dist/node/moduleRunnerTransport.d-CXw_Ws6P'
 const toast = useToast()
-const sysConfig = ref<sysConfig>(structuredClone(defaultSysConfig))
+const systemConfigLoading = ref(false)
+const sysConfig = ref<SystemConfigDto['SystemConfigController/DEFAULT_SYSTEM_CONFIG']>({
+    id: 0,
+    host: '',
+    showProfile: false,
+    showCopyRight: false
+})
 onMounted(async () => {
-    sysConfig.value = await getSysConfig()
+    await refreshSystemConfig()
 })
 
+async function refreshSystemConfig() {
+    systemConfigLoading.value = true
+    sysConfig.value = await api.systemConfigController.getSystemConfig()
+    systemConfigLoading.value = false
+}
+
+const loginName = ref('')
 const oldPassword = ref('')
 const newPassword = ref('')
 const confirmPassword = ref('')
 const passwordloading = ref(false)
 
 async function updateConfig() {
-    sysConfig.value.loading = true
-    const resp = await patchSysConfig(sysConfig.value)
+    systemConfigLoading.value = true
+    const resp = await api.systemConfigController.updateSystemConfig({
+        body: {
+            host: sysConfig.value.host,
+            showProfile: sysConfig.value.showProfile,
+            showCopyRight: sysConfig.value.showCopyRight
+        }
+    })
     if (resp) {
         toast.add({
             severity: 'success',
@@ -36,11 +57,21 @@ async function updateConfig() {
             life: 3000
         })
     }
-    sysConfig.value.loading = false
+    systemConfigLoading.value = false
 }
 
 async function updatePassword() {
     passwordloading.value = true
+    // if (loginName.value.trim() === '') {
+    //     toast.add({
+    //         severity: 'error',
+    //         summary: '更新失败',
+    //         detail: '登录名不能为空',
+    //         life: 3000
+    //     })
+    //     passwordloading.value = false
+    //     return
+    // }
     if (newPassword.value !== confirmPassword.value) {
         toast.add({
             severity: 'error',
@@ -51,23 +82,53 @@ async function updatePassword() {
         passwordloading.value = false
         return
     }
-    const resp = await changePassword(oldPassword.value, newPassword.value)
-    if (resp === 'success') {
+    try {
+        await api.profileController.updatePassword({
+            update: {
+                loginName: loginName.value,
+                oldPassword: oldPassword.value,
+                newPassword: newPassword.value
+            }
+        })
+
         toast.add({
             severity: 'success',
             summary: '更新成功',
             detail: '退出登录后生效',
             life: 3000
         })
-    } else {
+    } catch (error: any) {
+        let err: ApiErrors['profileController']['updatePassword']
+        if (error instanceof Promise) {
+            err = (await error) as ApiErrors['profileController']['updatePassword']
+        } else {
+            err = error as ApiErrors['profileController']['updatePassword']
+        }
+        console.log(err)
+        let detail = ''
+        switch (err.code) {
+            case 'EMPTY_LOGIN_NAME':
+                detail = '登录名不能为空'
+                break
+            case 'FORBIDDEN':
+                detail = '旧密码错误'
+                break
+            case 'SYSTEM_UNINITIALIZED':
+                detail = '系统未初始化'
+                break
+            default:
+                detail = '未知错误'
+                break
+        }
         toast.add({
             severity: 'error',
             summary: '更新失败',
-            detail: resp,
-            life: 20000
+            detail: detail,
+            life: 3000
         })
+    } finally {
+        passwordloading.value = false
     }
-    passwordloading.value = false
 }
 </script>
 <template>
@@ -76,31 +137,37 @@ async function updatePassword() {
         label="站点域名"
         placeholder="当前版本无须设置此项，留空即可"
         v-model="sysConfig.host"
-        :loading="sysConfig.loading"
+        :loading="systemConfigLoading"
     />
     <div class="flex">
         <LabelAndCheckbox
             title="是否展示着陆页最下方的版权信息"
             id="showCopyright"
             label="仓库地址"
-            v-model="sysConfig.show_copyright"
-            :loading="sysConfig.loading"
+            v-model="sysConfig.showCopyRight"
+            :loading="systemConfigLoading"
         />
         <LabelAndCheckbox
             title="是否展示着陆页信息，如果不展示则只显示登录表单"
             id="showProfilePage"
             label="展示主页"
-            v-model="sysConfig.show_profile_page"
-            :loading="sysConfig.loading"
+            v-model="sysConfig.showProfile"
+            :loading="systemConfigLoading"
         />
     </div>
 
-    <div class="flex justify-end mt-4 mb-4">
-        <Button @click="updateConfig" :loading="sysConfig.loading" label="保存" />
+    <div class="mt-4 mb-4 flex justify-end">
+        <Button @click="updateConfig" :loading="systemConfigLoading" label="保存" />
     </div>
 
     <Card class="lg:w-1/2">
         <template #content>
+            <LabelAndInput
+                id="loginName"
+                label="登录名"
+                v-model="loginName"
+                :loading="passwordloading"
+            />
             <LabelAndInput
                 id="oldPassword"
                 label="旧密码"
@@ -121,7 +188,7 @@ async function updatePassword() {
             />
         </template>
         <template #footer>
-            <div class="flex justify-end mt-4">
+            <div class="mt-4 flex justify-end">
                 <Button @click="updatePassword" :loading="passwordloading" label="更新" />
             </div>
         </template>
