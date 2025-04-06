c75a6312a8084e2b4b547918d9ef6766a51bcaea
YangYiFan <coolfan1024@gmail.com>
2025-04-06 19:58:44 +0800

    chore: 为深度克隆工具函数添加注释

--- src/utils/CloneWithCallback.ts
+++ src/utils/CloneWithCallback.ts
@@ -1,3 +1,12 @@
+/**
+ * 深度克隆一个对象，优先使用structuredClone，不支持时回退到JSON序列化方案
+ * @param obj 需要克隆的对象
+ * @returns 返回克隆后的新对象
+ * @example
+ * const original = { a: 1, b: { c: 2 } };
+ * const clone = cloneWithFallback(original);
+ * // clone 是一个全新的对象，修改clone不会影响original
+ */
 export function cloneWithFallback<T>(obj: T): T {
     if (typeof structuredClone === 'function') {
         return structuredClone(obj)
