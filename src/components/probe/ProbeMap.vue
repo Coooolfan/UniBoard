b74a74864130058ef606f6228615c37d1d32f9c0
YangYiFan <coolfan1024@gmail.com>
2025-08-10 02:56:03 +0800

    Switch to SVG map of Iceland and update chart to effectScatter

--- src/components/probe/ProbeMap.vue
+++ src/components/probe/ProbeMap.vue
@@ -1,123 +1,90 @@
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue'
 import * as echarts from 'echarts/core'
-import { TitleComponent, GeoComponent } from 'echarts/components'
-import { CanvasRenderer } from 'echarts/renderers'
-import { ScatterGLChart } from 'echarts-gl/charts'
-import worldMap from '@/assets/world.json'
-echarts.use([TitleComponent, GeoComponent, ScatterGLChart, CanvasRenderer])
-echarts.registerMap('world', worldMap as any)
+import {
+    TooltipComponent,
+    type TooltipComponentOption,
+    GeoComponent,
+    type GeoComponentOption
+} from 'echarts/components'
+import { EffectScatterChart, type EffectScatterSeriesOption } from 'echarts/charts'
+import { UniversalTransition } from 'echarts/features'
+import { CanvasRenderer, SVGRenderer } from 'echarts/renderers'
+import icelandSvg from '@/assets/Map_of_Iceland.svg?raw'
+
+echarts.use([
+    TooltipComponent,
+    GeoComponent,
+    EffectScatterChart,
+    CanvasRenderer,
+    UniversalTransition,
+    SVGRenderer
+])
+
+type EChartsOption = echarts.ComposeOption<
+    TooltipComponentOption | GeoComponentOption | EffectScatterSeriesOption
+>
+
 const map = ref<HTMLElement | null>(null)
 let myChart: echarts.ECharts | null = null
-
-// const ROOT_PATH = 'https://echarts.apache.org/examples'
-const ROOT_PATH = ''
-const CHUNK_COUNT = 230
-let dataCount = 0
-
-function fetchData(idx: number) {
-    if (idx >= CHUNK_COUNT) {
-        return
-    }
-    const dataURL = ROOT_PATH + '/data/asset/data/gps/gps_' + idx + '.bin'
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', dataURL, true)
-    xhr.responseType = 'arraybuffer'
-    xhr.onload = function (e) {
-        const rawData = new Int32Array(this.response)
-        const data = new Float32Array(rawData.length)
-        const addedDataCount = rawData.length / 2
-        for (let i = 0; i < rawData.length; i += 2) {
-            data[i] = rawData[i + 1] / 1e7
-            data[i + 1] = rawData[i] / 1e7
-        }
-        if (myChart) {
-            myChart.appendData({
-                seriesIndex: 0,
-                data: data
-            })
-        }
-        fetchData(idx + 1)
-    }
-    xhr.send()
-}
-
-function fetchMockProbeTargets() {
-    const mockData = [
-        { lng: 116.4074, lat: 39.9042 }, // Beijing
-        { lng: 121.4737, lat: 31.2304 }, // Shanghai
-        { lng: 114.0579, lat: 22.5431 }, // Shenzhen
-        { lng: 113.2644, lat: 23.1291 } // Guangzhou
-    ]
-    const data = new Float32Array(mockData.length * 2)
-    mockData.forEach((item, index) => {
-        data[index * 2] = item.lng
-        data[index * 2 + 1] = item.lat
-    })
-    if (myChart) {
-        myChart.appendData({
-            seriesIndex: 0,
-            data: data
-        })
-        // myChart.setOption({ geo: { zoom: 1.2 } })
-    }
-}
 
 onMounted(() => {
     if (!map.value) return
 
-    myChart = echarts.init(map.value)
+    myChart = echarts.init(map.value, null, { renderer: 'svg' })
 
     if (!myChart) return
 
-    const option = {
-        backgroundColor: '#f2f2f2',
+    echarts.registerMap('iceland_svg', { svg: icelandSvg })
+
+    const option: EChartsOption = {
+        tooltip: {},
         geo: {
-            map: 'world',
+            map: 'iceland_svg',
             roam: true,
-            label: {
-                emphasis: {
-                    show: false
-                }
-            },
-            silent: true,
-            itemStyle: {
-                normal: {
-                    areaColor: '#fff',
-                    borderColor: '#ccc'
-                }
-            }
+            zoom: 1,
+            // center: ['50%', '50%'],
+            // boundingCoords: [
+            //     // 定位左上角经纬度
+            //     [-180, 90],
+            //     // 定位右下角经纬度
+            //     [180, -90]
+            // ]
         },
-        series: [
-            {
-                name: '弱',
-                type: 'scatterGL',
-                progressive: 1e6,
-                coordinateSystem: 'geo',
-                symbol: 'circle',
-                symbolSize: 1,
-                zoomScale: 10,
-                blendMode: 'lighter',
-                large: true,
-                itemStyle: {
-                    color: 'rgb(20, 15, 2)'
-                },
-                postEffect: {
-                    enable: true
-                },
-                silent: true,
-                dimensions: ['lng', 'lat'],
-                data: new Float32Array()
-            }
-        ]
+        series: {
+            type: 'effectScatter',
+            coordinateSystem: 'geo',
+            geoIndex: 0,
+            symbolSize: function (params) {
+                return (params[2] / 100) * 15 + 10
+            },
+            itemStyle: {
+                color: '#b02a02'
+            },
+            encode: {
+                tooltip: 2
+            },
+            data: [
+                [-180, 90, 0],
+                [488.2358421078053, 459.70913833075736, 1],
+                [770.3415644319939, 757.9672194986475, 2],
+                [1180.0329284196291, 743.6141808346214, 3],
+                [894.03790632245, 1188.1985153835008, 4],
+                [1372.98925630313, 477.3839988649537, 5],
+                [1378.62251255796, 935.6708486282843, 6],
+                [180, -90, -1],
+            ]
+        }
     }
 
     myChart.setOption(option)
-    // fetchData(0)
-    fetchMockProbeTargets()
+
+    myChart.getZr().on('click', function (params) {
+        const pixelPoint = [params.offsetX, params.offsetY]
+        const dataPoint = myChart?.convertFromPixel({ geoIndex: 0 }, pixelPoint)
+        console.log(dataPoint)
+    })
 })
-
-
 
 onUnmounted(() => {
     if (myChart) {
